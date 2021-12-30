4f2d562c7fc730d69a36717c948885aacdcea4f8
humberto-desk <humberto_araiza@outlook.com>
2021-12-30 10:25:39 -0800

    cap 74

--- models/busquedas.js
+++ models/busquedas.js
@@ -8,10 +8,26 @@ class Busquedas {
         // TODO: leer DB si existe
     }
 
+    get paramsMapbox() {
+        return {
+            'access_token': 'pk.eyJ1IjoiaGFyYWl6YSIsImEiOiJja3h0OXVrN2QzZzM0MnZvY3lkdXMweHk3In0.pO1Y9onlgYenk_omWSYGVA',
+            'limit': 5,
+            'language': 'es'
+        }
+    }
+
     async ciudad(lugar = '') {
 
-        try {                             
-            const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/madrid.json?language=es&access_token=pk.eyJ1IjoiaGFyYWl6YSIsImEiOiJja3h0OXVrN2QzZzM0MnZvY3lkdXMweHk3In0.pO1Y9onlgYenk_omWSYGVA&limit=5');
+        try {
+
+            const instance = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+                params: this.paramsMapbox
+            });
+
+            const resp = await instance.get();
+
+            //const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/madrid.json?language=es&access_token=pk.eyJ1IjoiaGFyYWl6YSIsImEiOiJja3h0OXVrN2QzZzM0MnZvY3lkdXMweHk3In0.pO1Y9onlgYenk_omWSYGVA&limit=5');
             console.log(resp.data);
             return [];
         }
